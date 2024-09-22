a074069251ef04859ce6d60924763701b24eef33
goraldilara@gmail.com <6628goral.>
2024-09-22 08:20:20 +0300

    DB connections has been made with JPARepository

--- src/main/java/dg/littleweather/domain/entity/HourlyData.java
+++ src/main/java/dg/littleweather/domain/entity/HourlyData.java
@@ -1,9 +1,39 @@
 package dg.littleweather.domain.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "hourly_data")
+@Getter
+@Setter
 public class HourlyData {
+
+    @Id
+    @NotNull
+    @Column(name= "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "city_weather_id")
+    private CityWeather cityWeather;
+
+    @Size(min = 1, max = 45)
+    @Column(name = "date_and_hour")
     private String dateAndHour;
+
+    @Column(name = "max_temp")
     private double max_temp;
+
+    @Column(name = "feels_like_temp")
     private double feels_like_temp;
+
+    @Column(name = "humidity_rate")
     private int humidity_rate;
 
     public HourlyData(){
@@ -17,11 +47,28 @@ public class HourlyData {
         this.humidity_rate = humidity_rate;
     }
 
-    public String getDateAndHour() {
+    @NotNull
+    public int getId() {
+        return id;
+    }
+
+    public void setId(@NotNull int id) {
+        this.id = id;
+    }
+
+    public CityWeather getCityWeather() {
+        return cityWeather;
+    }
+
+    public void setCityWeather(CityWeather cityWeather) {
+        this.cityWeather = cityWeather;
+    }
+
+    public @Size(min = 1, max = 45) String getDateAndHour() {
         return dateAndHour;
     }
 
-    public void setDateAndHour(String dateAndHour) {
+    public void setDateAndHour(@Size(min = 1, max = 45) String dateAndHour) {
         this.dateAndHour = dateAndHour;
     }
 
